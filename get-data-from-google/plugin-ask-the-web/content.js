6503a675e61b8a928b5288bd011ff2906ec11eb5
ArturLOtt <arturlocateli72@gmail.com>
2021-01-10 18:06:10 -0200

    this shit is getting hard is hard

---
+++ get-data-from-google/plugin-ask-the-web/content.js
@@ -0,0 +1,16 @@
+//var elts = document.getElementById('kp-wp-tab-cont-overview');
+//if(elts !== 'null') {
+//
+//	for (var i = 0; i < elts.length; i++) {
+//	  elts[i].style['background-color'] = '#F0C';
+//	} 
+//}
+//var hubs = document.getElementsByTagName('h2')
+//for (var i = 0; i < hubs.length; i++) {
+//  hubs[i].style['background-color'] = '#F0C';
+//}
+
+var hubs = document.getElementsByClassName('mod')
+for (var i = 0; i < hubs.length; i++) {
+  hubs[i].style['background-color'] = '#F0C';
+}
